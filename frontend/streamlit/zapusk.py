c5bc13641fe9b2ac49ee42beb0fbb2c2c4ec9e00
Stepan Ukolov <124688716+Y1OV@users.noreply.github.com>
2024-06-16 06:51:03 +0300

    Create zapusk.py

---
+++ frontend/streamlit/zapusk.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import streamlit_antd_components as sac
+from pages_ import pages
+
+
+
+def main() -> None:
+    # if 'index' not in st.session_state:
+    #     st.session_state['index'] = 0
+
+    with st.sidebar.container():
+        menu = sac.menu(
+            items=list(pages.keys()),
+            # index=st.session_state['index'],
+            index=1,
+            open_all=True,
+            size='middle',
+            format_func=lambda page: pages[page].title,
+        )
+        st.sidebar.info("Made with ❤ by the ikanam")
+
+    with st.container():
+        if menu in pages:
+            pages[menu].method()
+
+
+if __name__ == "__main__":
+    main()
